23010ba9fd873bd46dc39bef7f82f0efeba3567b
skiff2140 <skiff2140@gmail.com>
2020-03-02 23:32:18 +0400

    add compact css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const concatCss    = require('gulp-concat-css');
+const cleanCSS     = require('gulp-clean-css');
 
 gulp.task('hello', function(done){
   console.log('Привет, мир!')
@@ -15,3 +19,36 @@ gulp.task('browser-sync', function() {
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+gulp.task('sass', function() {
+
+  return gulp.src("src/sass/*.sass")
+  
+  .pipe(sass().on('error', sass.logError))
+  
+  .pipe(autoprefixer({
+  
+  browsers: ['last 2 versions'],
+  
+  cascade: false
+  
+  }))
+.pipe(concatCss("style.css"))
+
+.pipe(gulp.dest("src/css"))
+
+.pipe(browserSync.stream());
+
+});
+
+gulp.task('mincss', function() {
+
+return gulp.src("src/css/*.css")
+
+.pipe(rename({suffix: ".min"}))
+
+.pipe(cleanCSS())
+
+.pipe(gulp.dest("app/css"));
+
+})
